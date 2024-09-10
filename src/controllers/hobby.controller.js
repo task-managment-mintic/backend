005eab6cdb52be32af78bb980568e20f40d046a8
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-10 01:02:37 -0500

    feat: add method to get hobbies fix: add async to create hobby method

--- src/controllers/hobby.controller.js
+++ src/controllers/hobby.controller.js
@@ -1,11 +1,11 @@
 import { Hobby } from "../models/hobby.model.js"
 
-export const createHobby = (req, res) => {
+export const createHobby = async (req, res) => {
     const { id } = req.user
     const { name, hobby_type } = req.body
 
     try {
-        const newHobby = Hobby.create({
+        const newHobby = await Hobby.create({
             name,
             hobby_type,
             user_id: id
@@ -16,5 +16,21 @@ export const createHobby = (req, res) => {
         })
     } catch (error) {
         return res.status(500).json({ message: 'Error creating hobby' })
+    }
+}
+
+export const getHobbies = async (req, res) => {
+    const { id } = req.user
+
+    try {
+        const hobbies = await Hobby.findAll({ where: { user_id: id } })
+        if (!hobbies) return res.status(404).json({ message: 'El usuario no tiene registrado hobbies' })
+        
+        return res.status(200).json({
+            message: 'Success',
+            hobbies: hobbies
+        })
+    } catch (error) {
+        return res.status(500).json({ message: 'Error getting hobbies' })
     }
 }
--- src/routes/hobby.routes.js
+++ src/routes/hobby.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createHobby } from '../controllers/hobby.controller.js'
+import { createHobby, getHobbies } from '../controllers/hobby.controller.js'
 import { authRequired } from '../middlewares/checkToken.js'
 import { validateSchema } from '../middlewares/validator.js'
 import { hobbySchema } from '../schemas/hobby.schema.js'
@@ -7,5 +7,6 @@ import { hobbySchema } from '../schemas/hobby.schema.js'
 const router = Router()
 
 router.post('/hobby', authRequired, validateSchema(hobbySchema), createHobby)
+router.get('/hobby', authRequired, getHobbies)
 
 export default router
